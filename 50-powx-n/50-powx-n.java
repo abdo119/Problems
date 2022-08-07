4ae415c055b857bc1e41a8e5c20a7c8bc30c4fb7
Abdelrahman <61024238+abdo119@users.noreply.github.com>
2022-08-07 23:22:08 +0200

    Time: 1 ms (89.32%), Space: 42.7 MB (63.97%) - LeetHub

--- 50-powx-n/50-powx-n.java
+++ 50-powx-n/50-powx-n.java
@@ -1,37 +1,5 @@
 class Solution {
     public double myPow(double x, int n) {
-      if(n==Integer.MIN_VALUE)
-        {
-            x=x*x;
-            n=Integer.MIN_VALUE+1;
-        }
-        
-        if(n==0)
-        {
-            return 1;
-        }
-        
-        
-        if(n<0)
-        {
-            x=1/x;
-            n=-n;
-        }
-        
-        
-        double xn1 = myPow(x,n/2);
-       
-        if(n%2!=0)
-        {
-           
-           return xn1*xn1*x; 
-        }
-        else
-        {
-            return xn1*xn1;
-        }
-        
-        
-        
+        return Math.pow(x,n);
     }
 }
