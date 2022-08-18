7680097d7c867e69f48eb20dad41d3c0a407aa35
Abdelrahman <61024238+abdo119@users.noreply.github.com>
2022-08-18 16:17:50 +0200

    Time: 117 ms (71.97%), Space: 19.9 MB (89.42%) - LeetHub

--- 15-3sum/15-3sum.cpp
+++ 15-3sum/15-3sum.cpp
@@ -1,37 +1,42 @@
 class Solution {
 public:
-  vector<vector<int>> threeSum(vector<int>& nums) {
-	std::vector<vector<int>> result;
-	if (nums.empty()) {
-		return result;
-	}
+   vector<vector<int>> threeSum(vector<int>& nums) {
+        sort(nums.begin() , nums.end());    //Sorted Array
+        if(nums.size() < 3){    //Base case 1
+            return {};
+        }
+        if(nums[0] > 0){        //Base case 2
+            return {};
+        }
+        vector<vector<int>> answer;
+        for(int i = 0 ; i < nums.size() ; ++i){     //Traversing the array to fix the number.
+            if(nums[i] > 0){     //If number fixed is +ve, stop there because we can't make it zero by searching after it.
+                break;
+            }
+            if(i > 0 && nums[i] == nums[i - 1]){    //If number is getting repeated, ignore the lower loop and continue.
+                continue;
+            }
+            int low = i + 1 , high = nums.size() - 1;   //Make two pointers high and low, and initialize sum as 0.
+            int sum = 0;
+            while(low < high){                          //Search between two pointers, just similiar to binary search.
+                sum = nums[i] + nums[low] + nums[high];
+                if(sum > 0){   //If sum is +ve, means, we need more -ve numbers to make it 0, decreament high (high--).
+                    high--;
+                } else if(sum < 0){ //If sum is -ve, means, we need more +ve numbers to make it 0, increament low (low++).
+                    low++;
+                } else {
+                    answer.push_back({nums[i] , nums[low] , nums[high]});  //we have found the required triplet, push it in answer vector
+                    int last_low_occurence = nums[low] , last_high_occurence = nums[high];  //Now again, to avoid duplicate triplets, we have to navigate to last occurences of num[low] and num[high] respectively
+                    while(low < high && nums[low] == last_low_occurence){   // Update the low and high with last occurences of low and high.
+                        low++;
+                    }
+                    while(low < high && nums[high] == last_high_occurence){
+                        high--;
+                    }
+                }
+            }
+        }
+        return answer;      //Return the answer vector.
+    }
 
-	std::size_t n_size = nums.size();
-	std::sort(nums.begin(), nums.end());
-	for (int i = 0; i < n_size; ++i) {
-		// all numbers from now on will be greater than 0, no point in continuing
-		if (nums[i] > 0) break;
-
-		// we have seen this number & combo before; skip
-		if (i > 0 and nums[i] == nums[i-1]) continue;
-
-		int left = i+1, right = n_size - 1;
-		while (left < right) {
-			int sum = nums[i] + nums[left] + nums[right];
-			if (sum < 0) {
-				++left;
-			} else if (sum > 0) {
-				--right;
-			} else {
-				result.push_back({nums[i], nums[left], nums[right]});
-				int last_left = nums[left], last_right = nums[right];
-				// we have seen this number & combo before; skip
-				while (left < right && nums[left] == last_left) ++left;
-				while (left < right && nums[right] == last_right) --right;
-			}
-		}
-
-	}
-	return result;
-}
 };
